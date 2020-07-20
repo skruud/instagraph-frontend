a7223bb0915f34574b9b8ed3627882599f658eaf
skruud <skruud@gmail.com>
2020-07-20 15:20:01 +0200

    Connect to backend

--- App.js
+++ App.js
@@ -8,16 +8,14 @@ const postInputY = document.querySelector('.post-input-y');
 const postButton = document.querySelector('.post-button');
 const postList = document.querySelector('.post-list');
 const chartType = document.querySelector('.pick-chart');
-
 const postPreview = document.querySelector('.preview-list');
 
 
 //Event Listeners
-//document.addEventListener('DOMContentLoaded', getPostList);
+document.addEventListener('DOMContentLoaded', getPostList);
 formButton.addEventListener('click', buttonsCheck);
-postButton.addEventListener('click', addPost);
+postButton.addEventListener('click', createPost);
 postList.addEventListener('click', buttonsCheck);
-//filterOption.addEventListener('click', filterPosts);
 postInput.addEventListener('click', updatePreview);
 postInputX.addEventListener('click', updatePreview);
 postInputY.addEventListener('click', updatePreview);
@@ -115,27 +113,93 @@ async function updatePreview(event) {
   
 }
 
+function buttonsCheck(e) {
+  const button = e.target;
+  //Check mark
+  if (button.classList[0] === 'like-btn') {
+    const postItem = button.parentElement;
+    const id = postItem.childNodes[1].innerText;
 
-async function addPost(event) {
+    console.log(postItem);
+    console.log(postItem.childNodes);
+    postItem.childNodes[3].classList.toggle('liked');
+    if (postItem.classList.contains('liked')) {
+      //putPost(id, true);
+    } 
+    else {
+      //putPost(id, false);
+    }
+  }
+  //Delete post
+  else if (button.classList[0] === 'delete-btn') {
+    const postItem = button.parentElement;
+    
+    console.log('DELETED');
+    //Animation
+    postItem.classList.add('fall');
+    postItem.addEventListener('transitionend', function() {
+      postItem.remove();
+    })
+    const id = postItem.childNodes[2].innerText;
+    deletePost(id);
+  }
+  //View form
+  else if (button.classList[0] === 'form-button') {
+    console.log('FORM BUTTON');
+    if (form.style.display === 'block') {
+      form.style.display = 'none';
+      button.innerHTML = '<i class="fas fa-plus-square"></i>';
+      if (postPreview.childNodes[0] !== undefined) {
+        postPreview.removeChild(postPreview.childNodes[0]);
+      }
+    }
+    else {
+      form.style.display = 'block';
+      button.innerHTML = '<i class="fas fa-minus-square"></i>';
+    }
+  }
+}
 
-  //Prevent form from submitting
-  event.preventDefault();
+//Get posts from server
+function getPostList() {
+  fetch('https://63fvwrrcug.execute-api.eu-north-1.amazonaws.com/dev/ig')
+    .then(response => response.json())
+    .then(data => 
+      importPosts(data)
 
-  
+    );
+}
+
+//Iterate through each post
+function importPosts(data) {
+  console.log(data);
+  for (const post of data) {
+    importPost(post);
+  }
+}
+
+//Add post to html
+function importPost(post) {
   //Post DIV
   const postDiv = document.createElement('div');
   postDiv.classList.add('post');
 
   //Create LI
   let newPost = document.createElement('li');
-  newPost.innerText = postInput.value;
+  newPost.innerText = post.text;
   newPost.classList.add('post-item');
   postDiv.appendChild(newPost);
 
 
-  let x_array = postInputX.value.split(",");
-  let y_array = postInputY.value.split(",");
+  let newElement = document.createElement('li');
+  newElement.innerText = post.id;
+  newElement.classList.add('post-id');
+  newElement.style.display = 'none';
+  postDiv.appendChild(newElement);
 
+  let x_array = post['x-array'][0];
+  let y_array = post['y-array'][0];
+  let chartType = post.chartType;
 
   let newGraph = document.createElement('canvas');
   newGraph.id = 'myChart2';
@@ -143,7 +207,7 @@ async function addPost(event) {
 
   var ctx2 = newGraph.getContext('2d');
   var myChart2 = new Chart(ctx2, {
-        type: chartType.value,
+        type: chartType,
         data: {
             labels: x_array,
             datasets: [{
@@ -197,150 +261,33 @@ async function addPost(event) {
 
   //Append to post list
   postList.appendChild(postDiv);
-
-  //Clear postInput
-  postInput.value = "";
-  postInputX.value = "";
-  postInputY.value = "";
   
-  if (postPreview.childNodes[0] !== undefined) postPreview.removeChild(postPreview.childNodes[0]);
 }
 
-function buttonsCheck(e) {
-  const button = e.target;
-  //Check mark
-  if (button.classList[0] === 'like-btn') {
-    const postItem = button.parentElement;
-    const id = postItem.childNodes[1].innerText;
-
-    console.log(postItem);
-    console.log(postItem.childNodes);
-    postItem.childNodes[3].classList.toggle('liked');
-    if (postItem.classList.contains('liked')) {
-      //putPost(id, true);
-    } 
-    else {
-      //putPost(id, false);
-    }
-  }
-  //Delete post
-  else if (button.classList[0] === 'delete-btn') {
-    const postItem = button.parentElement;
-    
-    console.log('DELETED');
-    //Animation
-    postItem.classList.add('fall');
-    postItem.addEventListener('transitionend', function() {
-      postItem.remove();
-    })
-    const id = postItem.childNodes[1].innerText;
-    //deletePost(id);
-  }
-  //Open form
-  else if (button.classList[0] === 'form-button') {
-    
-    if (form.style.display === 'block') {
-      form.style.display = 'none';
-      button.innerHTML = '<i class="fas fa-plus-square"></i>';
-      if (postPreview.childNodes[0] !== undefined) {
-        postPreview.removeChild(postPreview.childNodes[0]);
-      }
-    }
-    else {
-      form.style.display = 'block';
-      button.innerHTML = '<i class="fas fa-minus-square"></i>';
-    }
-  }
+//Get post data from form 
+async function createPost(event) {
+  event.preventDefault();
+  const x_array = postInputX.value.split(",");
+  const y_array = postInputY.value.split(",");
+  console.log('postPost Function');
+  const data = { userName: 'admin', text: postInput.value, 'x-array': [x_array], 'y-array': [y_array], chartType: chartType.value };
+  //const data = { text: postInput.value };
+  let postData = await postPost(data);
 }
 
-function filterPosts(e) {
-  const posts = postList.childNodes;
-  for (const post of posts) {
-    switch (e.target.value) {
-      case 'all':
-        post.style.display = 'flex';
-        break;
-      case 'liked':
-        post.classList.contains('liked') ? 
-          post.style.display = 'flex' : post.style.display = 'none';
-        break;
-      case 'unliked':
-        !post.classList.contains('liked') ? 
-          post.style.display = 'flex' : post.style.display = 'none';
-        break;
-    }
-  }
-}
+//Upload post to server
+function postPost(postData) {
 
-function getPostList() {
-  fetch('https://6zm55pojjf.execute-api.eu-north-1.amazonaws.com/dev/posts')
-    .then(response => response.json())
-    .then(data => 
-      importPosts(data)
-
-    );
-}
-
-function importPosts(data) {
-  console.log(data);
-  for (const post of data) {
-    importPost(post);
-  }
-}
-
-function importPost(post) {
-  const title = post.title;
-  //Post DIV
-  const postDiv = document.createElement('div');
-  postDiv.classList.add('post');
-
-  //Create LI
-  let newPost = document.createElement('li');
-  newPost.innerText = title;
-  newPost.classList.add('post-item');
-  postDiv.appendChild(newPost);
-
-  newPost = document.createElement('li');
-  newPost.innerText = post.id;
-  newPost.classList.add('post-id');
-  newPost.style.display = 'none';
-  postDiv.appendChild(newPost);
-
-  //CHECK MARK BUTTON
-  const likeButton = document.createElement('button');
-  likeButton.innerHTML = '<i class="fas fa-check">';
-  likeButton.classList.add('like-btn');
-  postDiv.appendChild(likeButton);
-  
-  //DELETE BUTTON
-  const deleteButton = document.createElement('button');
-  deleteButton.innerHTML = '<i class="fas fa-trash">';
-  deleteButton.classList.add('delete-btn');
-  postDiv.appendChild(deleteButton);
-
-  if (post.like == true) postDiv.classList.toggle('liked');
-
-  //Append to post list
-  postList.appendChild(postDiv);
-
-  //Clear postInput
-  postInput.value = "";
-}
-
-function postPost(title) {
-  const data = { title: title };
-  var id;
-
-  return fetch('https://6zm55pojjf.execute-api.eu-north-1.amazonaws.com/dev/posts', {
+  return fetch('https://63fvwrrcug.execute-api.eu-north-1.amazonaws.com/dev/ig', {
     method: 'POST', // or 'PUT'
     headers: {
       'Content-Type': 'application/json',
     },
-    body: JSON.stringify(data),
+    body: JSON.stringify(postData),
   })
   .then(response => response.json())
   .then( function(data)  {
-    return data.id;
+    return data;
   })
   .catch((error) => {
     console.error('Error:', error);
@@ -348,7 +295,7 @@ function postPost(title) {
 }
 
 function putPost(id, liked) {
-  const data = { like: liked };
+  const data = { likes: liked };
 
   fetch(`https://6zm55pojjf.execute-api.eu-north-1.amazonaws.com/dev/posts/${id}`, {
     method: 'PUT', // or 'PUT'
@@ -368,7 +315,7 @@ function putPost(id, liked) {
 
 function deletePost(id) {
 
-  fetch(`https://6zm55pojjf.execute-api.eu-north-1.amazonaws.com/dev/posts/${id}`, {
+  fetch(`https://63fvwrrcug.execute-api.eu-north-1.amazonaws.com/dev/ig/${id}`, {
     method: 'DELETE', // or 'PUT'
     headers: {
       'Content-Type': 'application/json',
